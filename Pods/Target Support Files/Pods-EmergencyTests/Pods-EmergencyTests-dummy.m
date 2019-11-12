19da8d19c735953da9524a7c11e6ca2ae2f28258
Patryk <patrykbresso@gmail.com>
2019-11-12 17:35:22 +0100

    Added Firebase connection

---
+++ Pods/Target Support Files/Pods-EmergencyTests/Pods-EmergencyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EmergencyTests : NSObject
+@end
+@implementation PodsDummy_Pods_EmergencyTests
+@end
---
+++ Pods/Target Support Files/Pods-EmergencyUITests/Pods-EmergencyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EmergencyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_EmergencyUITests
+@end
