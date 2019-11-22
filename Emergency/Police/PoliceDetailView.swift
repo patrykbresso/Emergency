ed5bdd4caf3eb3b21e6d15cb850358429fd43093
Patryk <patrykbresso@gmail.com>
2019-11-22 18:33:59 +0100

    Added working PoliceView

--- Emergency/Police/PoliceDetailView.swift
+++ Emergency/Police/PoliceDetailView.swift
@@ -15,34 +15,60 @@ struct PoliceDetailView: View {
 
     var body: some View {
         VStack {
+           
+                
             MapView(coordinate: policeStation.locationCoordinate)
-                .frame(height: 300)
-
+                .offset(y: 0)
+                .frame(width: 346, height: 270, alignment: .center)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.black, lineWidth: 3)
+                )
             CircleImage(image: Image(policeStation.imageName))
-                .offset(y: -130)
+                .offset(y: -100)
                 .padding(.bottom, -130)
 
             VStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(red: 219 / 255, green: 2 / 255, blue: 109 / 255), lineWidth: 10)
+                .background(Color(red: 219 / 255, green: 2 / 255, blue: 109 / 255))
+                .frame(width: 170, height: 120)
+                .offset(y: 40)
                 Text(policeStation.name)
-                    .font(.title)
-
-                HStack(alignment: .top) {
-                    Text(policeStation.address)
-                        .font(.subheadline)
-                    Spacer()
-                    Text(policeStation.email)
-                        .font(.subheadline)
-                }
+                    .padding(.top, 15)
+                    .padding(.leading, 5)
+                    .padding(.trailing, 5)
+                    .foregroundColor(.white)
+                    .offset(y: -100)
+                Text(policeStation.address)
+                    .padding(.top, 15)
+                    .padding(.leading, 5)
+                    .padding(.trailing, 5)
+                    .foregroundColor(.white)
+                    .offset(y: -115)
+                Text(policeStation.number)
+                    .padding(.top, 15)
+                    .padding(.leading, 5)
+                    .padding(.trailing, 5)
+                    .foregroundColor(.white)
+                    .offset(y: -130)
+                Text(policeStation.email)
+                    .padding(.top, 15)
+                    .padding(.leading, 5)
+                    .padding(.trailing, 5)
+                    .foregroundColor(.white)
+                    .offset(y: -145)
+                
             }
-            .padding()
-
-            Spacer()
-        }
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(Color(red: 19 / 255, green: 42 / 255, blue: 122 / 255))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
-struct ContentView_Preview: PreviewProvider {
+struct PoliceDetailView_Preview: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        PoliceDetailView(policeStation: policeData[1])
     }
 }
+
--- Emergency/Police/PoliceRow.swift
+++ Emergency/Police/PoliceRow.swift
@@ -15,13 +15,43 @@ struct PoliceRow: View {
 
     var policeStation: Police
     var body: some View {
+        
         List{
             VStack {
+                RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(red: 219 / 255, green: 2 / 255, blue: 109 / 255), lineWidth: 10)
+                .background(Color(red: 219 / 255, green: 2 / 255, blue: 109 / 255))
+                .frame(width: 170, height: 80)
+                .offset(y: 0)
+                
                 Text(policeStation.name)
+                .padding(.top, 20)
+                .padding(.leading, 5)
+                .padding(.trailing, 5)
+                .offset(y: -110)
                 Text(policeStation.address)
+                .padding(.top, 15)
+                .padding(.leading, 5)
+                .padding(.trailing, 5)
+                .offset(y: -125)
                 Text(policeStation.number)
+                .padding(.top, 15)
+                .padding(.leading, 5)
+                .padding(.trailing, 5)
+                .offset(y: -140)
                 Text(policeStation.email)
-            }
+                .padding(.top, 15)
+                .padding(.leading, 5)
+                .padding(.trailing, 5)
+                .offset(y: -155)
+                
+                
+            }.listRowBackground(Color(red: 19 / 255, green: 42 / 255, blue: 122 / 255))
+                .foregroundColor(.white)
+                .offset(x: 0)
+                //.background(Color(red: 19 / 255, green: 42 / 255, blue: 122 / 255))
+            //.listRowBackground(Color(red: 19 / 255, green: 42 / 255, blue: 122 / 255))
+            //.colorMultiply(Color(red: 19 / 255, green: 42 / 255, blue: 122 / 255)).padding(.top)
         }
     }
 }
