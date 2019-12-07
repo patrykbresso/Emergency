1b5a77fae4bdd5644524a23226ce8a93f9454957
PaulinaBinas <binaspaulina1@gmail.com>
2019-12-07 21:53:08 +0100

    Improvement of Help view

--- Emergency/Views/Help/HelpView.swift
+++ Emergency/Views/Help/HelpView.swift
@@ -6,60 +6,79 @@
 //  Copyright © 2019 Wasko. All rights reserved.
 //
 
+
+//Spacer()
+//TextField("WYSZUKAJ", text: self.$keyword)
+//    .textFieldStyle(CustomTextFieldStyle())
+//    .padding(.top, 20)
+//    .padding(.bottom, 20)
+//    .padding(.leading, 35)
+//    .padding(.trailing, 35)
+
 import SwiftUI
 import MessageUI
 
 struct HelpView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     @State private var keyword: String = ""
     @State var texts: [String] = ["emergency@pwr.edu"]
     @State var result: Result<MFMailComposeResult, Error>? = nil
     @State var isShowingMailView = false
     var boxHeight: CGFloat = 100
     
-    
     var body: some View {
-        VStack(spacing: 20) {
-            HStack{
-                Image("help")
-                Spacer()
-            }
-            .padding(.top, 30)
-            Spacer()
-            Button(action: {
-                self.onBoxClick(boxNo: 0)
-            }) {
-                VStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color(red: 219 / 255, green: 2 / 255, blue: 109 / 255), lineWidth: 10)
-                        .background(Color(red: 219 / 255, green: 2 / 255, blue: 109 / 255))
-                        .frame(width: 250, height: boxHeight)
-                    Text(texts[0])
-                        .padding(.top, 15)
-                        .foregroundColor(.white)
-                        .frame(width: 250, height: boxHeight)
-                        .offset(y: -120)
-                }
-            }.frame(width: 250, height: boxHeight)
-                .offset(y: 55)
-                .disabled(!MFMailComposeViewController.canSendMail())
-                .sheet(isPresented: $isShowingMailView) {
-                    MailView(result: self.$result)
-                }
-            Spacer()
-            TextField("WYSZUKAJ", text:$keyword)
-                .textFieldStyle(CustomTextFieldStyle())
-                .padding(.top, 20)
-                .padding(.bottom, 20)
-                .padding(.leading, 35)
-                .padding(.trailing, 35)
+        GeometryReader { geometry in
+            VStack(alignment: HorizontalAlignment.leading) {
                 
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    VStack {
+                        Image("help")
+                            .renderingMode(.original)
+                        Image("left_arrow")
+                            .renderingMode(.original)
+                        
+                    }.padding(.leading, 15)
+                }
+                Spacer()
+                Button(action: {
+                    self.onBoxClick(boxNo: 0)
+                }) {
+                    VStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color(red: 219 / 255, green: 2 / 255, blue: 109 / 255), lineWidth: 10)
+                            .background(Color(red: 219 / 255, green: 2 / 255, blue: 109 / 255))
+                            .frame(width: 250, height: self.boxHeight)
+                        Text(self.texts[0])
+                            .padding(.top, 15)
+                            .foregroundColor(.white)
+                            .frame(width: 250, height: self.boxHeight)
+                            .offset(y: -120)
+                    }
+                }.frame(width: 250, height: self.boxHeight)
+                    .offset(y: 55)
+                    .disabled(!MFMailComposeViewController.canSendMail())
+                    .sheet(isPresented: self.$isShowingMailView) {
+                        MailView(result: self.$result)
+                    }
+                    .padding(.trailing, (geometry.size.width - 250) / 2)
+                    .padding(.leading, (geometry.size.width - 250) / 2)
+                Spacer()
+                TextField("WYSZUKAJ", text: self.$keyword)
+                    .textFieldStyle(CustomTextFieldStyle())
+                    .padding(.top, 20)
+                    .padding(.bottom, 20)
+                    .padding(.leading, 35)
+                    .padding(.trailing, 35)
+            }
         }
-        .padding(.leading, 35)
-        .padding(.trailing, 35)
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-        .background(Color(red: 19 / 255, green: 42 / 255, blue: 122 / 255))
-        .foregroundColor(.white)
-        .edgesIgnoringSafeArea(.all)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(Color(red: 19 / 255, green: 42 / 255, blue: 122 / 255)
+            .edgesIgnoringSafeArea(.all)
+        )
+        .navigationBarBackButtonHidden(true)
     }
     
     func onBoxClick(boxNo: Int) {
