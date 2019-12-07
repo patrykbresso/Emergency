134d77d46e62e216b98c86d3ab77794b7a0eb25e
PaulinaBinas <binaspaulina1@gmail.com>
2019-12-07 21:07:39 +0100

    Improved detailed Difficult Situations view

--- Emergency/Views/DifficultSituations/DifficultSituationsDetailView.swift
+++ Emergency/Views/DifficultSituations/DifficultSituationsDetailView.swift
@@ -9,16 +9,49 @@
 import SwiftUI
 
 struct DifficultSituationsDetailView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var difficultSituation: DifficultSituations
     
     var body: some View {
-        VStack {
-            Text(difficultSituation.title)
-                .font(.title)
-                .padding(.bottom, 15)
-            Text(difficultSituation.text)
-            .padding(15)
+        GeometryReader { geometry in
+            VStack {
+                HStack {
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }) {
+                        VStack {
+                            Image("difficultsituations")
+                                .renderingMode(.original)
+                            Image("left_arrow")
+                                .renderingMode(.original)
+                            
+                        }.padding(.leading, 15)
+                    }
+                    Spacer()
+                }
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color(red: 219 / 255, green: 2 / 255, blue: 109 / 255), lineWidth: 10)
+                    .background(Color(red: 219 / 255, green: 2 / 255, blue: 109 / 255))
+                    VStack {
+                        Text(self.difficultSituation.title)
+                            .font(.title)
+                            .padding(.bottom, 15)
+                        Text(self.difficultSituation.text)
+                        .padding(15)
+                    }
+                }
+            .padding(30)
+            }
         }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(Color(red: 19 / 255, green: 42 / 255, blue: 122 / 255)
+            .edgesIgnoringSafeArea(.all)
+        )
+            .foregroundColor(.white)
+        .navigationBarBackButtonHidden(true)
     }
 }
 
